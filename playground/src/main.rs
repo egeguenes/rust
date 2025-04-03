3f036ca385b920fe9b80d7e6cb66720139c49c84
Ege Güneş <egunes@Mac.fritz.box>
2025-04-03 23:42:13 +0200

    borrow ownership

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -3,15 +3,25 @@ struct Potato {
     weight: i32,
 }
 
+struct Drink {
+    flavor: Flavor,
+    fluid_oz: f64,
+}
+
+struct GroceryItem {
+    id: i32,
+    quantity: i32,
+}
+
 enum Flavor {
     Sparkling,
     Sweet,
     Fruity,
 }
 
-struct Drink {
-    flavor: Flavor,
-    fluid_oz: f64,
+enum UniMember {
+    Student,
+    Professor,
 }
 
 fn print_drink(drink: Drink) {
@@ -30,6 +40,29 @@ impl std::fmt::Display for Potato {
     }
 }
 
+fn introduce(mem: UniMember) {
+    match mem {
+        UniMember::Student => println!("I am a student!"),
+        UniMember::Professor => println!("I am a professor!"),
+    }
+    // at the function exit the mem is deletedas the introduce function gets the ownership of the object from main function
+}
+
+fn introduce_borrow(mem: &UniMember) {
+    match mem {
+        UniMember::Student => println!("I am a student in the borrow function!"),
+        UniMember::Professor => println!("I am a professor in the borrow function!"),
+    }
+}
+
+fn display_id(item: &GroceryItem) {
+    println!("The id of the item is {:?}", item.id);
+}
+
+fn display_quantity(item: &GroceryItem) {
+    println!("The quantity of the item is {:?}", item.quantity);
+}
+
 fn main() {
     let my_potato = Potato {
         variety: String::from("Sweet Potato"),
@@ -38,6 +71,7 @@ fn main() {
 
     println!("{}\n\n", my_potato);
 
+    let _flavors = [Flavor::Fruity, Flavor::Sparkling, Flavor::Sweet];
     let energy = Drink {
         flavor: Flavor::Sparkling,
         fluid_oz: 12.24,
@@ -49,4 +83,30 @@ fn main() {
     let (x, y) = (2, 3);
     println!("\n\n({},{})", coord.0, coord.1);
     println!("({},{}\n\n", x, y);
+
+    let three = 3;
+    let magic_number = match three {
+        3 => "Magic number is three",
+        4 => "Magic number is four",
+        _ => "Magic number is neither three nor four",
+    };
+    /*
+        let is_three = three == 3;
+    */
+    println!("Magic number should be 3\n{}\n", magic_number);
+
+    let student = UniMember::Student;
+    introduce(student);
+    //introduce(student); Cant call this one, as the first call gets the ownership and the member was deleted in the first call.
+
+    let professor = UniMember::Professor;
+    introduce_borrow(&professor);
+    introduce_borrow(&professor);
+
+    let apples: GroceryItem = GroceryItem {
+        id: 1,
+        quantity: 13,
+    };
+    display_id(&apples);
+    display_quantity(&apples);
 }
